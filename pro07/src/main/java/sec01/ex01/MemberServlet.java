9b49b538d22a2a5026f3470301b54ab3e9d683f6
Minsoo Kim <alstndhffla@gmail.com>
2021-04-27 16:27:09 +0900

    update

---
+++ pro07/src/main/java/sec01/ex01/MemberServlet.java
@@ -0,0 +1,37 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/member")
+protected class MemberServlet extends HttpServlet{
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		MemberDAO dao = new MemberDAO();	//sql문으로 조회할 MemberDAO 객체 생성
+		List<MemberVO> list = dao.listMembers();	//listMember()메서드로 회원 정보를 조회한다.
+		
+		out.print("<html><body>");
+		out.print("<table border=1><tr align='center' bgcolor='lightgreen'>");
+		out.print("<td>아이디</td><td>비밀번호</td><td>이름</td><td>이메일</td><td>가입일</td></tr>");
+		
+		//조회한 회원 정보를 for문과 tr태그를 이용해 리스트로 출력한다.
+		for(int i = 0; i < list.size(); i++) {
+			MemberVO memberVO = list.get(i);
+			String id = memberVO.getId();
+			String pwd = memberVO.getPwd();
+			String name = memberVO.getName();
+			String email = memberVO.getEmail();
+			Date joinDate = memberVO.getJoinDate();
+			out.print("<tr><td>"+id+"</td><td>"+pwd+"</td><td>"+name+"</td><td>"+email+"</td><td>"+joinDate+"</td><tr>");
+		}
+		out.print("</table></body></html>");
+	}
+}
